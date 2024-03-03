2eb0bb47725fc2c7cb6f4c9bb41affdb9a94cc07
Arnabh <sinhaarnabh888@gmail.com>
2024-03-03 11:20:33 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import express from "express";
 
-const express=require("express");
+
 const app=express();
 
 app.use(cors({
@@ -11,10 +12,16 @@ app.use(cors({
 }));
 
 app.use(express.json({limit:"16kb"}));
-app.use(express.urlenoed({extended:true,limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declartaion
+app.use("/api/v1/users", userRouter);
 
 
 export default app;
